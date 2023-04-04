6fd31894dd062c65af6afc91b075b36ce19619cb
rdanf2 <reylet.daniel@gmail.com>
2023-04-04 23:26:20 +0200

    2d commit kotlin commit (no readme)

--- A.kt
+++ A.kt
@@ -8,6 +8,13 @@
  * From 1st "Simple example: Reachable Numbers problem" : A.kt file
  * https://kotlinlang.org/docs/competitive-programming.html
  **/
+
+/* DR : Just... Readme 
+    0. copy this code online
+    1. set an integer in field form at https://play.kotlinlang.org/
+    2. click Run... 
+*/
+
 tailrec fun removeZeroes(x: Int): Int =
     if (x % 10 == 0) removeZeroes(x / 10) else x
 
@@ -21,9 +28,9 @@ fun main(args: Array<String>) {
         n = f(n); // iterate function f
         println(n);                   //-/DR-addition for understanding
         println(reached.sorted()); }  //-/DR-addition 
-                //-/ (sorted - HashSet sort as text by default : 1 200 3 400 ...)
+                //-/ (sorted - HashSet sort/add/insert as text by default : 1 200 3 400 ...)
     
     println(reached.size) // print answer to the output
+         //-/ Means => How many iterations of f() before reaching a previously reached integer
 }
-/* Just 1. set an integer and 2. click Run... */
 
